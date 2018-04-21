2c95362afe36d2ac5499048b9283a53bec1ca18e
fzning <zhanningFan@126.com>
2018-04-22 02:32:03 +0800

    session 15
    child component sends message to father component

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -3,10 +3,16 @@ import CommentInput from './CommentInput'
 import CommentList from './CommentList'
 
 class CommentApp extends Component {
+
+    //声明一个回调函数，最终会在子组件CommentInput的作用域内被调用执行，相当于通过该函数去接收子组件传递的数据，实现父子组件通信
+    handleSubmitComment(comment) {
+        console.log(comment)
+    }
     render() {
         return (
             <div className='wrapper'>
-              <CommentInput />
+            {/* 通过props给子组件CommentInput传递class CommentApp中定义的函数作为回调函数 */}
+              <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
               <CommentList />
             </div>
         )
--- src/CommentInput.js
+++ src/CommentInput.js
@@ -18,6 +18,19 @@ class CommentInput extends Component {
             content: event.target.value
         })
     }
+    handleSubmit() {
+        //监听onSubmit，如果父组件有使用名为onSubmit的prop，就执行传入的回调函数，并将userName，content作为入参传过去
+        if (this.props.onSubmit) {
+            const {userName, content} = this.state
+            this.props.onSubmit({
+                userName,
+                content
+            })
+        }
+        this.setState({
+            content: ''
+        })
+    }
     render() {
         return (
             <div className='comment-input'>
@@ -38,7 +51,7 @@ class CommentInput extends Component {
                     </div>
                 </div>
                 <div className='comment-field-button'>
-                    <button>发布</button>
+                    <button onClick={this.handleSubmit.bind(this)}>发布</button>
                 </div>
             </div>
         )
